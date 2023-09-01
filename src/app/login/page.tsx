15b36f6096724e87ad86c9305b0f1e367454aa55
Vu Hoang Trung - Frontend <hoangtrung@mobcec.com>
2023-09-01 16:58:01 +0700

    +Add: setup project with nextjs 13

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import Button from "../components/Button/Button";
+
+export default function Login() {
+  return (
+    <div className="container">
+      <form action="">
+        <Button className="border rounded-lg h-12 w-full font-medium duration-200 ">ABC</Button>
+      </form>
+    </div>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,47 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
+  darkMode: 'class',
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx}'
   ],
+
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    container: {
+      center: true,
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1440px'
       },
+      padding: {
+        DEFAULT: '15px',
+      }
+    },
+    extend: {
+      fontSize: {
+        10: '10px',
+        12: '12px',
+        14: '14px',
+        16: '16px',
+        18: '18px',
+        20: '20px',
+        22: '22px',
+        24: '24px',
+        26: '26px',
+        28: '28px',
+        30: '30px',
+        32: '32px',
+        34: '34px',
+        36: '36px',
+        38: '38px',
+        40: '40px',
+        48: '48px'
+      }
     },
   },
   plugins: [],
 }
+
 export default config
